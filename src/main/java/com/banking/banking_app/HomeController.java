aaf4426498cc83dd5a8ab6eaf22c482eea5202d7
kabinanb <balakrishnankabinan@gmail.com>
2025-09-21 20:39:16 +0100

    Printing hello

---
+++ src/main/java/com/banking/banking_app/HomeController.java
@@ -0,0 +1,13 @@
+package com.banking.banking_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Banking App!";
+    }
+}
